dbca2d8eee225a845d82afb626e2d3302c3411da
jakobfechner312 <jfechner1994@gmail.com>
2026-01-30 00:06:16 +0100

    Remove nested AppShell on home page

--- src/app/(app)/page.tsx
+++ src/app/(app)/page.tsx
@@ -1,15 +1,9 @@
 // src/app/page.tsx
-import AppShell from "@/components/AppShell";
 import TagesberichtForm from "@/app/(app)/reports/new/TagesberichtForm";
 
 
 export default function Home() {
   return (
-    <AppShell
-      title="Tagesbericht"
-      subtitle="Drillexpert • Digitaler Tagesbericht"
-    >
-      <TagesberichtForm />
-    </AppShell>
+    <TagesberichtForm />
   );
 }
