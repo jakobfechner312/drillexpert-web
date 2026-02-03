e8990fae238dff7005fc49982ef2243a68a7d897
jakobfechner312 <jfechner1994@gmail.com>
2026-02-03 13:06:29 +0100

    Force dynamic reports new page

--- src/app/(app)/reports/new/page.tsx
+++ src/app/(app)/reports/new/page.tsx
@@ -1,5 +1,7 @@
 import TagesberichtForm from "@/app/(app)/reports/new/TagesberichtForm";
 
+export const dynamic = "force-dynamic";
+
 export default function NewReportPage() {
   return <TagesberichtForm mode="create" />;
 }
